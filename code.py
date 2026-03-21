7522dcc4de08f281b1adb6348c88aaa366e76ab5
Shreya-Bhat-2006 <iamshreyabhat@gmail.com>
2026-03-21 22:52:27 +0530

    edited code.py

--- code.py
+++ code.py
@@ -6,3 +6,13 @@ df=pd.read_csv("Data.csv")
 df.drop('customerID',axis="columns",inplace=True)
 df=df[df.TotalCharges!=" "]
 df.TotalCharges=pd.to_numeric(df.TotalCharges)
+df.replace('No internet service','No',inplace=True)
+df.replace('No phone service','No',inplace=True)
+
+def print_unique_col_values(df):
+    for column in df:
+        if df[column].dtypes == 'object':
+            print(f'{column} : {df[column].unique()}')
+
+# Call the function with your dataframe
+print_unique_col_values(df)
