304dfd6ccf9dff242c660403dc3099095964cf14
Shreya-Bhat-2006 <iamshreyabhat@gmail.com>
2026-03-22 22:00:00 +0530

    edited code.py

--- code.py
+++ code.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
+
+ 
 df=pd.read_csv("Data.csv")
 df.drop('customerID',axis="columns",inplace=True)
 df=df[df.TotalCharges!=" "]
@@ -14,5 +17,18 @@ def print_unique_col_values(df):
         if df[column].dtypes == 'object':
             print(f'{column} : {df[column].unique()}')
 
-# Call the function with your dataframe
-print_unique_col_values(df)
+yes_no_columns = ['Partner','Dependents','PhoneService','MultipleLines', 'OnlineSecurity','OnlineBackup',
+'DeviceProtection','TechSupport','StreamingTV','StreamingMovies' ,'PaperlessBilling' ,'Churn']
+
+
+for col in yes_no_columns:
+    df[col] = df[col].map({"Yes": 1, "No": 0})
+
+df["gender"] = df["gender"].map({"Male": 0, "Female": 1})
+df = pd.get_dummies(df, columns=["InternetService", "Contract", "PaymentMethod"], dtype=int)
+
+col_to_scale=["tenure","MonthlyCharges","TotalCharges"]
+
+Scalar=MinMaxScaler()
+df[col_to_scale]=Scalar.fit_transform(df[col_to_scale])
+print(df.head())
